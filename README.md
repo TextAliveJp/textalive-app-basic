c64761e4abd6f61606ca98cd195d3a4a0ec874d9
Jun Kato <i@junkato.jp>
2020-09-17 23:30:08 +0900

    Add documentations

---
+++ README.md
@@ -0,0 +1,43 @@
+# TextAlive App API basic example
+
+API チュートリアル「1. 開発の始め方」のサンプルコードです。
+発声中の歌詞を単語単位で表示します。
+また、このアプリが TextAlive ホストと接続されていなければ再生コントロールを表示します。
+
+- API チュートリアル「1. 開発の始め方」: https://developer.textalive.jp/app/
+- サンプルコードのデモページ: https://textalivejp.github.io/textalive-app-basic/
+
+## 違う楽曲で試すには
+
+TextAlive App API で開発されたWebアプリケーションは、（特定の楽曲向けに作り込んでいない限り）URLのクエリパラメタで `ta_song_url={楽曲のURL}` を指定すると異なる楽曲で演出を試せます。
+
+- [ブレス・ユア・ブレス by 和田たけあき feat. 初音ミク](https://textalivejp.github.io/textalive-app-basic/?ta_song_url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Da-Nf3QUFkOU)
+- [グリーンライツ・セレナーデ by Omoi feat. 初音ミク](https://textalivejp.github.io/textalive-app-basic/?ta_song_url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DXSLhsjepelI)
+
+## 開発
+
+[Node.js](https://nodejs.org/) をインストールしている環境で以下のコマンドを実行すると、開発用サーバが起動します。
+
+```sh
+npm install
+npm run dev
+```
+
+## ビルド
+
+以下のコマンドで `docs` 以下にビルド済みファイルが生成されます。 [サンプルコードのデモページ](https://textalivejp.github.io/textalive-app-basic/) は [GitHub Pages](https://pages.github.com/) で、このリポジトリの `docs` 以下のファイルが提供されています。
+
+```sh
+npm run build
+```
+
+## TextAlive App API
+
+![TextAlive](https://i.gyazo.com/thumb/1000/5301e6f642d255c5cfff98e049b6d1f3-png.png)
+
+TextAlive App API は、音楽に合わせてタイミングよく歌詞が動くWebアプリケーション（リリックアプリ）を開発できるJavaScript用のライブラリです。
+
+TextAlive App API について詳しくはWebサイト [TextAlive for Developers](https://developer.textalive.jp/) をご覧ください。
+
+---
+https://github.com/TextAliveJp/textalive-app-basic
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-// TextAlive App API basic example
-// https://github.com/TextAliveJp/textalive-app-basic
-
-// see also: https://github.com/TextAliveJp/textalive-app-phrase
+/**
+ * TextAlive App API basic example
+ * https://github.com/TextAliveJp/textalive-app-basic
+ * 
+ * API チュートリアル「1. 開発の始め方」のサンプルコードです。
+ * 発声中の歌詞を単語単位で表示します。
+ * また、このアプリが TextAlive ホストと接続されていなければ再生コントロールを表示します。
+ * https://developer.textalive.jp/app/
+ */
 
 import { Player } from "textalive-app-api";
 
-// 
+// 単語が発声されていたら #text に表示する
+// Show words being vocalized in #text
 const animateWord = function (now, unit) {
   if (unit.contains(now)) {
     document.querySelector("#char").textContent = unit.text;
   }
 };
 
+// TextAlive Player を作る
+// Instantiate a TextAlive Player instance
 const player = new Player({
   app: {
     appAuthor: "Jun Kato",
@@ -19,8 +27,9 @@ const player = new Player({
   },
   mediaElement: document.querySelector("#media"),
 });
-console.log(player);
 
+// TextAlive Player のイベントリスナを登録する
+// Register event listeners
 player.addListener({
   onAppReady,
   onVideoReady,
@@ -37,9 +46,15 @@ const positionEl = document.querySelector("#position strong");
 const artistSpan = document.querySelector("#artist span");
 const songSpan = document.querySelector("#song span");
 
+/**
+ * TextAlive App が初期化されたときに呼ばれる
+ * 
+ * @param {IPlayerApp} app - https://developer.textalive.jp/packages/textalive-app-api/interfaces/iplayerapp.html
+ */
 function onAppReady(app) {
 
-  // show control when the app is launched standalone
+  // TextAlive ホストと接続されていなければ再生コントロールを表示する
+  // Show control if this app is launched standalone (not connected to a TextAlive host)
   if (!app.managed) {
     document.querySelector("#control").style.display = "block";
     playBtn.addEventListener("click", () => player.video && player.requestPlay());
@@ -47,18 +62,26 @@ function onAppReady(app) {
     rewindBtn.addEventListener("click", () => player.video && player.requestMediaSeek(0));
   }
 
-  // load a song when a song URL is not specified
+  // 楽曲URLが指定されていなければ マジカルミライ 2020テーマ曲を読み込む
+  // Load a song when a song URL is not specified
   if (!app.songUrl) {
-      player.createFromSongUrl("http://www.youtube.com/watch?v=ygY2qObZv24");
+    player.createFromSongUrl("http://www.youtube.com/watch?v=ygY2qObZv24");
   }
 }
 
+/**
+ * 動画オブジェクトの準備が整ったとき（楽曲に関する情報を読み込み終わったとき）に呼ばれる
+ * 
+ * @param {IVideo} v - https://developer.textalive.jp/packages/textalive-app-api/interfaces/ivideo.html
+ */
 function onVideoReady(v) {
-  // show meta data
+  // メタデータを表示する
+  // Show meta data
   artistSpan.textContent = player.data.song.artist.name;
   songSpan.textContent = player.data.song.name;
 
-  // set "animate" function
+  // 定期的に呼ばれる各単語の "animate" 関数をセットする
+  // Set "animate" function
   let w = player.video.firstWord;
   while (w) {
     w.animate = animateWord;
@@ -66,13 +89,22 @@ function onVideoReady(v) {
   }
 }
 
+/**
+ * 動画の再生位置が変更されたときに呼ばれる（あまりに頻繁な発火を防ぐため一定間隔に間引かれる）
+ * 
+ * @param {number} position - https://developer.textalive.jp/packages/textalive-app-api/interfaces/playereventlistener.html#onthrottledtimeupdate
+ */
 function onThrottledTimeUpdate(position) {
-  // update current position
+  // 再生位置を表示する
+  // Update current position
   positionEl.textContent = String(Math.floor(position));
 
-  // more precise timing information can be retrieved by `player.timer.position` at any time
+  // さらに精確な情報が必要な場合は `player.timer.position` でいつでも取得できます
+  // More precise timing information can be retrieved by `player.timer.position` at any time
 }
 
+// 再生が一時停止・停止したら歌詞表示をリセット
+// Reset lyrics text field when music playback is paused or stopped
 function onPause() {
   document.querySelector("#char").textContent = "-";
 }
